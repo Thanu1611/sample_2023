1b6f6e4c06d2bb7251e7185f02dc75e167174699
Thedsanamoorthysarma Thanushika <121013173+Thanu1611@users.noreply.github.com>
2023-01-13 09:01:52 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ class sum
     public static void main(String args[])
     {
         int a=10;
-        int b=12;
+        int b=12; //Some change made in comments
         System.out.println(a+b);
     }
 }
